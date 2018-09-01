bb2b10a54dc32168dba5ca0583a99998f2e2d36d
Mo'men Soliman <moumensoliman@gmail.com>
2018-09-01 03:54:17 +0200

    Add base files

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import GithubRepos from './components/github/GithubRepos.vue'
+
+Vue.component('GithubRepos', GithubRepos);
+
+export {GithubRepos}
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './App.vue'
+import {GithubRepos} from './index.js'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
